74e66bfa624b4e1203495f4ef8ac363bd863d8ab
POk346 <petrenko.ok2016@yandex.ru>
2021-07-16 18:12:36 +0300

    2_Lesson

--- GB_2_Lesson/main.swift
+++ GB_2_Lesson/main.swift
@@ -2,10 +2,49 @@
 //  main.swift
 //  GB_2_Lesson
 //
-//  Created by Oksana on 15.07.2021.
+//  Created by Oksana on 16.07.2021.
 //
 
 import Foundation
 
-print("Hello, World!")
+
+//1. Написать функцию, которая определяет, четное число или нет
+
+func separatуTheEvenNumber(number1: Int) {
+    if (number1 % 2 == 0) {
+        print("\(number1) - четное число")
+    } else {
+        print("\(number1) - нечетное число")
+    }
+}
+separatуTheEvenNumber(number1: 155)
+
+
+//2. Написать функцию, которая определяет, делится ли число без остатка на 3
+
+func numberDivisibleByThree(numberByTree: Int) {
+    if (numberByTree % 3 == 0) {
+        print("\(numberByTree) - число делится на 3 без остатка")
+    } else {
+        print("\(numberByTree) - число не делится на 3 без остатка")
+    }
+}
+numberDivisibleByThree(numberByTree: 126)
+
+
+//3. Создать возрастающий массив из 100 чисел
+
+var someArray = [Int] (1...100)
+print(someArray)
+
+//4. Удалить из этого массива все четные и все числа, которые не делятся на 3
+
+var filterNuber = someArray.filter {$0 % 2 != 0 && $0 % 3 == 0}
+
+print(filterNuber)
+
+
+
+
+
 
